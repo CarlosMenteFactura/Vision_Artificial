d540d978b6c61447523d674ae71881a4b59b6af2
CarlosMenteFactura <carlos.mendoza@mentefactura.mx>
2023-10-25 16:38:35 -0600

    subida del codigo a github

---
+++ DatosConexion.py
@@ -0,0 +1,7 @@
+"""Datos del servidor deonde esta la base de datos"""
+Datos={
+       "User":"code",
+       "Pwd":"1234",
+       "nameServer":"DESKTOP-QNMCN6L\SQLEXPRESS",
+       "nameDB":"Brocolis"
+       }
